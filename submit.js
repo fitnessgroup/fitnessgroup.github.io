51439b1c31b3a507a2e1882f32c83015b8397b75
yguo <yguo@yuelong-guos-macbook-pro.local>
2015-04-09 23:33:33 -0400

    added log functionality

---
+++ submit.js
@@ -0,0 +1,25 @@
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+var send = function(){
+	var json = JSON.stringify($('form').serializeObject())
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "https://api.mongolab.com/api/1/databases/fitnessdb/collections/fitnessrecord?apiKey=7IhjRbwiIXy1Y2kOl6HE2LiNqQf_LQEl", true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(json);
+    window.alert("Thanks! Your activity has been successfully logged.");
+}
